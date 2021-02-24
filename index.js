b0b0b02a9a856cb84daeab8413577ab39b6789b9
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-02-24 23:52:23 +0600

    push with new Branch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs"); // imports the file io library
 
 const client = new Discord.Client(); // creates a discord client
 const token = fs.readFileSync("token.txt").toString(); // gets your token from the file
- 
+  
 client.once("ready", () => { // prints "Ready!" to the console once the bot is online
 	console.log("Ready!");
 });
