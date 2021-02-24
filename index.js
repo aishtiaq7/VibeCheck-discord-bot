c9d0fd1d34add67a01b189a3a3b6b4576c229781
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-02-24 23:41:52 +0600

    added vibecheck me command

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js"); // imports the discord library
+const fs = require("fs"); // imports the file io library
+
+const client = new Discord.Client(); // creates a discord client
+const token = fs.readFileSync("token.txt").toString(); // gets your token from the file
+
+client.once("ready", () => { // prints "Ready!" to the console once the bot is online
+	console.log("Ready!");
+});
+
+client.login(token); // starts the bot up
+
+
+client.on("message", message => { // runs whenever a message is sent
+    if (message.content === "Vibecheck me") { // checks if the message says "?random"
+        const vibeCheckString = "VibeCheck yoo"; // generates a random number
+        message.channel.send(vibeCheckString); // sends a message to the channel with the number
+    }
+});
