85063a1859f478987df8504c465f7c6832b39464
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-04-25 14:46:13 +0600

    log msgs + code clean up

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const fetch = require("node-fetch");
-
 const Discord = require("discord.js"); // imports the discord library
 const fs = require("fs"); // imports the file io library
-
 const client = new Discord.Client(); // creates a discord client
+
 const token = fs.readFileSync("token.txt").toString(); // gets your token from the file
 
 //Import other node modules:
 let {Person} = require('./vibecheck_scoreCard.js') ;
 const { type } = require("os");
-//var person1 = new Person('Awshaf');
-//console.log(person1);
 
 
-
-//			-------- FIREBASE -------------
-
+//---------------- FIREBASE ------------- : Function definition at the bottom
 var firebase = require('firebase');
-
 var firebaseConfig = {
 	apiKey: "AIzaSyBJ8-p0-jZUEKFx5eKnpHgYOxBQ4dwcvr4",
     authDomain: "vibecheck-discord-bot.firebaseapp.com",
@@ -28,20 +22,16 @@ var firebaseConfig = {
     appId: "1:946523489068:web:bf6dc1cab8fafbd9922289",
     measurementId: "G-5Y9H17PS13"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
-//	      -------- FIREBASE ------------- Function definition at the bottom
-
+//---------------- FIREBASE config end
 
 
 
 client.once("ready", () => { 
 	
-	console.log('deployed from mainFeature - branch..')
-	
+	console.log('deployed from mainFeature - branch..') // WARNING: its hard coated
 	
 	console.log("Ready!");
 	console.log('__npm start at:');
@@ -58,6 +48,7 @@ client.once("ready", () => {
 
 client.login(token); // starts the bot up 
 
+
 var vibeCheckers =[];
 var vibecheckIsActive = false; // tracker for vibecheck window to allow ppl to vibecheck
 
@@ -68,7 +59,6 @@ var timeoutValueInMs = 12 * ( 60 * 1000 ) ; // window to responds to 'vibecheck 
 
 function start() {
 	startTime = new Date();
-	
 };
 
 function elapsedTimeForScore() {
@@ -81,13 +71,13 @@ function elapsedTimeForScore() {
 
 	timeScore /= 1000; //strip ms;
 
-	//timeScore <-- now it contains the elapsed time in SECONDS
-
-	//Converting it to score of 100:
-	// console.log('timeScore', timeScore);
-	// console.log('timeoutValueInMs',timeoutValueInMs);
-	// console.log('timeScore/timeoutVaueInMs',timeScore/timeoutValueInMs );
-	// console.log('Math.floor( (timeScore/timeoutValueInMs) *100 )', Math.floor( (timeScore/timeoutValueInMs) *100 ));
+	/*
+			mins to seconds
+			1s = 1000ms
+			1m = 60 x 1000ms 
+			15m = 15 x ( 60 x 1000 )ms
+			
+	*/
 
 	var score = Math.floor( (1 -(timeScore/ (timeoutValueInMs/1000)) ) *100 );
 	
@@ -127,7 +117,7 @@ function end() {
 	// var days = timeDiff ;
 }
 
- 
+
 //******************************** MESSAGES ********************************
 client.on("message", message => { // runs whenever a message is sent
 
@@ -136,7 +126,7 @@ client.on("message", message => { // runs whenever a message is sent
 	textMessage = textMessage.toLowerCase();
 
 	if (textMessage === "test".toLocaleLowerCase()) {
-		console.log("Function call -  test ____!_ by:",message.author.username);
+		console.log("Function call -  test ____!_ by:", message.author.username);
 		//saveWindowEntry(message);
 		
 	}
@@ -144,18 +134,13 @@ client.on("message", message => { // runs whenever a message is sent
 
     if (textMessage === "Vibecheck me".toLocaleLowerCase() ) {
 		
-		
 		console.log("Function call -  Vibecheck me ____!_ by:", message.author.username)
 		//Vibechecks wither username
 		const username = message.author.username;
         const vibeCheckString = "VibeChecked " + username; // generates a random number
         message.channel.send(vibeCheckString); // sends a message to the channel with the number
 
-		//react with an emoji
-		// const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === ':punch:');
 		message.react('👋🏼');
-		// message.react();
-		
 
     }
 
@@ -166,7 +151,6 @@ client.on("message", message => { // runs whenever a message is sent
 
 	if ( textMessage === "pl table".toLocaleLowerCase() ){
 
-		//Testing function calls:
 		plTableCommandFunction()
 		.then( response =>{
 			console.log('after function call log:');
@@ -180,8 +164,6 @@ client.on("message", message => { // runs whenever a message is sent
 		.catch(err =>{
 			console.log(err);
 		})
-
-
 	} 
 
 	if ( textMessage === "ucl".toLocaleLowerCase() ){
@@ -191,8 +173,6 @@ client.on("message", message => { // runs whenever a message is sent
 		uclFetchFunction()
 		.then((response) => {
 
-			// console.log("console logging response");
-			// console.log(response);
 			var matchString ="@Bangladesh Standard Time:\n";
 			response.matches.forEach(element => {
                 if( element.status == "SCHEDULED"){
@@ -219,23 +199,11 @@ client.on("message", message => { // runs whenever a message is sent
 
 	if ( textMessage === "Vibecheck @everyone".toLocaleLowerCase() ){
 
-		console.log("vc now - function call ______!_ by:", message.author.username)
+		console.log("vc @everyone - function call ______!_ by:", message.author.username)
 		vibecheckIsActive = true;
 		
-
 		start(); // starts clock 
 
-				
-		/*
-			mins to seconds
-			1s = 1000ms
-			1m = 60 x 1000ms 
-			15m = 15 x ( 60 x 1000 )ms
-			
-		*/
-
-		// const timeoutValueInMs = 25 * ( 60 * 1000 );
-		// timeoutValueInMs = 11000; *******GLOBAL VALUE ****** 
 		setTimeout( ()=>{
 			vibecheckIsActive = false;
 			end();
@@ -248,8 +216,7 @@ client.on("message", message => { // runs whenever a message is sent
 
 		message.channel.send(`Vibecheck window closes in:${timeoutValueInMs/1000/60}min`);
 
-		//saveWindowEntry(message);
-		
+		//saveWindowEntry(message); TODO: remove the code for saving data locally to SavedData.txt
 	}
 
 	if ( textMessage === "vibin".toLocaleLowerCase() ){
@@ -270,8 +237,7 @@ client.on("message", message => { // runs whenever a message is sent
 	}
 });
 
-function changeTimeZone( dateToChange){
-
+function changeTimeZone( dateToChange){ //Changes time to BANGLADESH TIME
 
     const changeThisDate = new Date(dateToChange);
     const options = {
@@ -284,13 +250,9 @@ function changeTimeZone( dateToChange){
         hour12: true,
     }
     const formatter = new Intl.DateTimeFormat('en-GB', options)
-    
     const dateInNewTimezone = formatter.format(changeThisDate) 
-
-    // console.log( 'dateInNewTimezone: ',dateInNewTimezone) // 12-04-10 17:10:30 GMT+7
     
     return dateInNewTimezone;
-    
 }
 
 function uclFetchFunction() {
@@ -306,7 +268,7 @@ function uclFetchFunction() {
 		.then(res => res.json())
 }
 function displayVibeCheckers(message){
-	//YOU HAVE vibeCheckers <---- this is an array containing the people who vibechecked
+	//vibeCheckers <---- this is an array containing the people who vibechecked
 	
 	if( vibeCheckers.length == 0){
 		message.channel.send('No one vibechecked in the given time :( ');
@@ -315,57 +277,47 @@ function displayVibeCheckers(message){
 
 	var replyString = 'Good vibes:\n';
 
-	// console.log('loggin vibeCheckers:');
-
 	vibeCheckers.forEach( person=> {
 		replyString += `${person.Name}\t\t\t${person.Score}pts\n`
 	})
 
 	console.log(replyString);
 	message.channel.send(replyString);
-
-
-
-
-
 }
 
-function saveDataToFirebase(message) {
+/*
+	Writes 'dataToWrite' json object to your 'reference' under 'childId' node. 
 
-			/*
-				Writes 'dataToWrite' json object to your 'reference' under 'childId' node. 
-
-				Pattern example: 
-				vibinScores{ // <---- reference
-						allEntries{
-							//123456: // <---- childID
-							{
-								...
-								..obj   <---- dataToWrite
-							}
-						}
+	Pattern example: 
+	vibinScores{ // <---- reference
+			allEntries{
+				//123456: // <---- childID
+				{
+					...
+					..obj   <---- dataToWrite
 				}
-			
-			function registerVibinScores( reference, childId, dataToWrite){
-			*/
-			var currentDate = new Date();
-			var vibinScoreObj = {
-				
-				viberId:message.author.id, //id
-				
-				person:new Person(  //person
-					message.author.username.toString(), //name 
-					elapsedTimeForScore(),	// score from window
-					message.author.id // user id
-					),
-					
-				timeOfEntry: currentDate.toString(), 
-
-				vibinScore:elapsedTimeForScore(),  //score
-
 			}
-			const path = 'vibinScores/allEntries';
-			saveEachVibinScores ( path, currentDate.getTime(), vibinScoreObj )
+	}
+*/
+function saveDataToFirebase(message) {
+	var currentDate = new Date();
+	var vibinScoreObj = {
+		
+		viberId:message.author.id, //id
+		
+		person:new Person(  //person
+			message.author.username.toString(), //name 
+			elapsedTimeForScore(),	// score from window
+			message.author.id // user id
+			),
+			
+		timeOfEntry: currentDate.toString(), 
+
+		vibinScore:elapsedTimeForScore(),  //score
+
+	}
+	const path = 'vibinScores/allEntries';
+	saveEachVibinScores ( path, currentDate.getTime(), vibinScoreObj )
 }
 
 //TODO: change the directory of the "SavedData.json" to a folder named data and save it there cuz
@@ -439,31 +391,16 @@ function saveWindowEntry(message){
 
 }
 
-/*
-	FORMAT: yyyy-m-d HH:MM:SS
-			2018-8-3 11:12:40
-
-function getCurrentDateAndTimeString() {
-	var today = new Date();
-	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-	var dateTime = date+' '+time;
-
-	// return dateTime;
-	
-}
-*/
 
 function registerVibecheck(message){
 	/*
 	Steps to follow:
 	-check if the time to vibecheck is still valid
 		if(valid){
-			push username + score to array
+			push (username + score) to array
 		}
 	-When vibecheck time runs out
 		Pop the array to display the vibecheckers with score
-
 	*/
 	
 	const username = message.author.username.toString();
@@ -482,21 +419,16 @@ function registerVibecheck(message){
 	
 
 	if ( !personExists ){
-		// console.log(`pushing ${person1.Name}`);
 		vibeCheckers.push(
 			person1
 		);
 	}
-
-	// console.log('logging vibeChecker array:');
-	// console.log(vibeCheckers);
-	
-
 }
 
-
+/*
+	Return Promise with Priemier League Table Top Teams
+*/
 function plTableCommandFunction(){
-
 
 	return fetch("http://api.football-data.org/v2/competitions/2021/standings", {
 		headers: { 'X-Auth-Token': '06d819b33cd245fc89707771ad9759a2' },
@@ -510,7 +442,7 @@ function plTableCommandFunction(){
 		var printArray = [];
 		var printString = "";
 		var count =10;  // Max num of top teams to be displayed
-		//WITH FOR LOOP
+
 		printArray.push('\tTeam\t\t\tPlayed | Pts')
 		for ( var i=0 ; i< count; i++){
 			printString = `${response.standings[0].table[i].position}:${response.standings[0].table[i].team.name}`;
@@ -521,25 +453,21 @@ function plTableCommandFunction(){
 			}
 			printString +=`${response.standings[0].table[i].playedGames} | ${response.standings[0].table[i].points}`;
 			
-			// console.log(printString);
 			printArray.push(printString);
-
-			
 		}
 		return printArray.join('\n');
 	})
-
 }
 
 
-
-
-var db = firebase.database();
+var db = firebase.database();  // FIREBASE Database Function Definitions
 
 /* **********************************************************
-                            READ
+						Firebase - READ
     **********************************************************
 */ 
+
+
 /*
     Reads data asynchronously with the 'path' argument specified. 
     RETURNING A PROMISE.
@@ -579,7 +507,7 @@ promise.then( dataReceived => {
 */
 
 /* **********************************************************
-                            Firebase- Write
+						Firebase - Write
     **********************************************************
 */ 
 /*
@@ -613,6 +541,3 @@ function saveEachVibinScores( reference, childId, dataToWrite){
     );
 
 }
-
-
-
